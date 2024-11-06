b15851b712831ac4df65bca028b608f293e03aea
suusanex <suusan@himado.net>
2024-11-06 11:17:14 +0900

    WPFにWinAppSDKを組み込み、自動初期化設定。アプリライフサイクルを使用して、2つ目以降のインスタンスの起動時に1つ目のインスタンスのメインウインドウをActivateする処理を実装。

---
+++ SampleWPFWinAppSDK/SampleWPFWinAppSDK/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Windows.ApplicationModel.DynamicDependency;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWPFWinAppSDK
+{
+    internal class Program
+    {
+
+        // NOTE We can't make Main async in a WPF app.
+        //[STAThread]
+        //public static int Main(string[] args)
+        //{
+        //    // Initialize Windows App SDK for unpackaged apps.            
+        //    int result = 0;
+        //    if (Bootstrap.TryInitialize(majorMinorVersion, out result))
+        //    {
+        //        bool isRedirect = DecideRedirection();
+        //        if (!isRedirect)
+        //        {
+        //            App app = new()
+        //            {
+        //                StartupUri = new Uri("MainWindow.xaml", UriKind.Relative)
+        //            };
+        //            app.Run();
+        //        }
+
+        //        // Uninitialize Windows App SDK.
+        //        Bootstrap.Shutdown();
+        //    }
+        //    return 0;
+        //}
+    }
+}
